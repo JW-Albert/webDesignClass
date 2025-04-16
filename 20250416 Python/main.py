7eaf376281311f6dfe9c7995716d2c09ec9e288b
JW-Albert <ru04jo30801@gmail.com>
2025-04-16 16:07:12 +0800

    feat: Initialize main.py with fruit sorting functionality and commented age classification logic

---
+++ 20250416 Python/main.py
@@ -0,0 +1,32 @@
+def main () -> int:
+    """age = 20
+
+    if ( age >= 18 ) :
+        print( "Adult" )
+    elif ( age >= 13 ) :
+        print( "Teen" )
+    else :
+        print( "Child" )"""
+
+    fruit = ["apple" ,"cherry" ,"banana" ]
+    fruit.sort()
+    print(fruit)
+
+    # for i in fruit :
+    #     print( i )
+    
+    # for i in range( 5 ) :
+    #     print( i )
+
+    # for i in range( 2 ,6 ) :
+    #     print( i )
+
+    # for i in range( 0 ,10 ,2 ) :
+    #     print( i )
+
+    return 0
+
+
+
+if ( __name__ == "__main__" ):
+    main()
